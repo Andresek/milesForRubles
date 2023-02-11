42c0a73935de9a7017373c83aac735ccdf14d98c
Andrew S <samum_94@mail.ru>
2023-02-11 16:25:39 +0300

    Main changed

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
-        int ticketPrice = 4585;
-        int oneBonusMile = 20;
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
-        int sumBonusMile = ticketPrice / oneBonusMile;
-        System.out.println(sumBonusMile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
